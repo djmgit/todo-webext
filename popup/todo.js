cde6ca63a71f6105a31c21f156e50ec61a50933b
djmgit <djmdeveloper060796@gmail.com>
2017-08-20 23:09:35 +0530

    Added source

---
+++ popup/todo.js
@@ -0,0 +1,103 @@
+$(document).ready(function() {
+	$(".add-note").hide();
+
+    $(".add").click(function() {
+	    $(".add-note").slideToggle();
+    });
+    
+    var notes = [];
+    
+    $(".submit").click(function() {
+	    var noteHead = $(".note-head").val();
+	    var noteBody = $(".note-body").val();
+
+	    $(".note-head").val("");
+	    $(".note-body").val("");
+
+	    if (!noteHead || !noteBody) {
+	    	return;
+	    }
+    
+	    var note = {
+		    "title": noteHead,
+		    "body": noteBody
+	    }
+    
+	    notes.unshift(note);
+    
+	    //storage = {}
+	    //storage["todo"] = notes;
+	    //browser.storage.local.set(storage);
+
+	    localStorage.todo = JSON.stringify(notes);
+    
+	    getNotes();
+	    $(".add-note").slideUp();
+    });
+    
+    function getNotes() {
+
+	    /*browser.storage.local.get("todo").then(function(storedNotes) {
+		    notes = storedNotes.todo;
+		    if (notes === undefined || notes.length === 0) {
+		        notes = [];
+		        $(".empty").fadeIn();
+		    } else {
+		    	$(".empty").fadeOut();
+		    }
+
+		    console.log(notes);
+
+		    updateNotes();
+	    });*/
+
+	    if (!localStorage.todo || localStorage.todo === "[]") {
+		    notes = [];
+		    $(".empty").fadeIn();
+		} else {
+		    $(".empty").fadeOut();
+		    notes = JSON.parse(localStorage.todo);
+		}
+
+		
+		updateNotes();
+    }
+    
+    function updateNotes() {
+	    var notesHtml = "";
+	    
+   
+	    notes.forEach(function(elem) {
+		    noteHtml = "<div class='card'>" +
+		                   "<div class='card-header'>" +
+		                       "<span><strong>" + elem.title + "</strong></span>" +
+		                       "<span class='remove'>" +
+		                           "<img src='remove.svg'>" +
+		                       "<span>" +
+		                   "</div>" +
+		                   "<div class='card-body'>" + elem.body + "</div>" +
+		               "</div>";
+		    notesHtml += noteHtml;
+	    });
+    
+	    $(".notes").html(notesHtml);
+
+	    $(".remove").click(function() {
+	    	var key = $(this).prev().text();
+	    	notes = notes.filter(function(item) {
+	    		return item["title"] !== key;
+	    	});
+	    	
+	    	//storage = {}
+	        //storage["todo"] = notes;
+	        //browser.storage.local.set(storage);
+
+	        localStorage.todo = JSON.stringify(notes);
+	        getNotes();
+	    });
+    }
+
+    getNotes();
+
+});
+
